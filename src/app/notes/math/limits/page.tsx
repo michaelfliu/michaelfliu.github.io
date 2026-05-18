b791108ea94a4b0b9381960afc3e7991bd15794e
Michael Liu <michaelf.liu@outlook.com>
2026-05-18 00:51:24 -0700

    Add string processing for katex rendering

--- src/app/notes/math/limits/page.tsx
+++ src/app/notes/math/limits/page.tsx
@@ -8,7 +8,7 @@ import ConditionalRenderList from "@/components/ConditionalRenderList";
 import ConditionalRenderListItem from "@/components/ConditionalRenderListItem";
 import MultiSelectButtons from "@/components/MultiSelectButtons";
 import SectionTitle from "@/components/SectionTitle";
-import { M, IM } from "@/components/MyKatex";
+import { M, IM, TypesetKatex } from "@/components/MyKatex";
 
 const PartTitle = styled(Typography)(() => ({
   fontWeight: "bold",
@@ -27,141 +27,153 @@ export default function LimitsPage() {
   const theme = useTheme();
   return (
     <PageContainer>
-      <PageTitle>Everything I Know About Limits</PageTitle>
-      <Box
-        my={theme.spacing(1)}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        width="100%"
-      >
-        <MultiSelectButtons
-          labels={Object.values(LBL)}
-          defaultLabels={visibleLabels}
-          onChange={setVisibleLabels}
-        />
-      </Box>
-      <Box display="flex" flexDirection="column" gap={theme.spacing(1)}>
-        <SectionTitle>Definitions</SectionTitle>
-        <ConditionalRenderList visibleLabels={visibleLabels}>
-          <ConditionalRenderListItem labels={[LBL.FN]}>
-            <p>
-              In the following definitions, let <IM m="p \in \R" /> be a point
-              and <IM m="f(x) : \R \to \R" /> be a function defined on some open
-              interval left to be specified. Where appropriate,{" "}
-              <IM m="x_0 \in \R" /> is the endpoint of an interval on which{" "}
-              <IM m="f" /> is defined.
-            </p>
-            <PartTitle>Limits at a Point</PartTitle>
-            <p>
-              Let <IM m="f" /> be defined on some open interval around, but not
-              necessarily including <IM m="p" />. We say that{" "}
-              <IM m="L \in \R" /> is the <i>limit</i> of <IM m="f" /> as{" "}
-              <IM m="x" /> approaches <IM m="p" /> if for any{" "}
-              <IM m="\epsilon > 0" /> there exists some <IM m="\delta > 0" /> so
-              that for any <IM m="x \neq p" /> and{" "}
-              <IM m="\abs{x - p} < \delta" /> we have{" "}
-              <IM m="\abs{f(x) - L} < \epsilon" />.
-            </p>
-            <PartTitle>One-Sided Limits</PartTitle>
-            <p>
-              Let <IM m="f" /> be defined on some open interval{" "}
-              <IM m="(x_0, p)" />. Then we say that <IM m="R \in \R" /> is the{" "}
-              <i>limit</i> of <IM m="f" /> as <IM m="x" /> approaches{" "}
-              <IM m="p" /> <b>from the left (or from below)</b> if for any{" "}
-              <IM m="\epsilon > 0" /> there exists some <IM m="\delta > 0" /> so
-              that for any <IM m="x < p" /> and <IM m="p - x < \delta" /> we
-              have <IM m="\abs{f(x) - R} < \epsilon" />.
-            </p>
-            <p>
-              Let <IM m="f" /> be defined on some open interval{" "}
-              <IM m="(p, x_0)" />. Then we say that <IM m="L \in \R" /> is the{" "}
-              <i>limit</i> of <IM m="f" /> as <IM m="x" /> approaches{" "}
-              <IM m="p" /> <b>from the right (or from above)</b> if for any{" "}
-              <IM m="\epsilon > 0" /> there exists some <IM m="\delta > 0" /> so
-              that for any <IM m="x > p" /> and <IM m="x - p < \delta" /> we
-              have <IM m="\abs{f(x) - L} < \epsilon" />.
-            </p>
-            <PartTitle>Limits at Infinity</PartTitle>
-            <p>
-              Let <IM m="f" /> be defined on some interval{" "}
-              <IM m="(x_0, \infty)" />. Then we say that <IM m="L \in \R" /> is
-              the <i>imit</i> of <IM m="f" /> as <IM m="x" /> approaches{" "}
-              <IM m="\infty" /> if for any <IM m="\epsilon > 0" /> there is some{" "}
-              <IM m="x_\epsilon \in \R" /> such that for all{" "}
-              <IM m="x > x_\epsilon" /> we have{" "}
-              <IM m="\abs{f(x) - L} < \epsilon" />
-            </p>
-            <p>
-              Let <IM m="f" /> be defined on some interval{" "}
-              <IM m="(-\infty, x_0)" />
-              . Then we say that <IM m="L \in \R" /> is the <i>limit</i> of{" "}
-              <IM m="f" /> as <IM m="x" /> approaches <IM m="-\infty" /> if for
-              any <IM m="\epsilon > 0" /> there is some{" "}
-              <IM m="x_\epsilon \in \R" /> such that for all{" "}
-              <IM m="x < x_\epsilon" /> we have{" "}
-              <IM m="\abs{f(x) - L} < \epsilon" />
-            </p>
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.SEQ]}>
-            <p>
-              Let <IM m="X = \set{x_n}" /> be a sequence of real numbers. We say
-              that <IM m="L \in \R" /> is the <i>limit</i> of <IM m="x" /> as{" "}
-              <IM m="n" /> approaches <IM m="\infty" /> and write{" "}
-              {`\[ \lim_{n\to\infty} x_n = L \]`} if for any{" "}
-              <IM m="\epsilon > 0" /> there is an <IM m="n_0 \in \N" /> such
-              that for all <IM m="n > n_0" /> we have{" "}
-              <IM m="\abs{x_n - L} < \epsilon" />.
-            </p>
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.MET]}>
-            <p>
-              The definition of the limit on a metric space <IM m="\cX" /> with
-              metric <IM m="d : \cX \times \cX \to \R" /> mirrors those
-              definitions for limits of regular functions and sequences/series.
-              Instead of assuming <IM m="d(x, y) = \abs{x - y}" /> as is the
-              case for <IM m="\R" /> , we allow <IM m="d" /> to be an arbitrary
-              metric.
-            </p>
-            <PartTitle>Functions</PartTitle>
-            <p>
-              Let <IM m="f" /> be defined on some open interval around, but not
-              necessarily including <IM m="p" />. We say that{" "}
-              <IM m="L \in \cX" /> is the <i>limit</i> of <IM m="f" /> as{" "}
-              <IM m="x" /> approaches <IM m="p" /> if for any{" "}
-              <IM m="\epsilon > 0" /> there exists some <IM m="\delta > 0" /> so
-              that for any <IM m="x \neq p" /> and{" "}
-              <IM m="\abs{x - p} < \delta" /> we have{" "}
-              <IM m="d(f(x), L) < \epsilon" />.
-            </p>
-            <PartTitle>Sequences</PartTitle>
-            <p>
-              Let <IM m="X = \set{x_n}" /> be a sequence of elements in{" "}
-              <IM m="\cX" />. We say that <IM m="L \in \cX" /> is the{" "}
-              <i>limit</i> of <IM m="x" /> as <IM m="n" /> approaches{" "}
-              <IM m="\infty" /> and write
-            </p>
-            <M m="\lim_{n\to\infty} x_n = L" />
-            <p>
-              if for any <IM m="\epsilon > 0" /> there is an{" "}
-              <IM m="n_0 \in \N" /> such that for all <IM m="n > n_0" /> we have{" "}
-              <IM m="d(x_n, L) < \epsilon" />.
-            </p>
-          </ConditionalRenderListItem>
-        </ConditionalRenderList>
+      <TypesetKatex>
+        <PageTitle>Everything I Know About Limits</PageTitle>
+        <Box
+          my={theme.spacing(1)}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+        >
+          <MultiSelectButtons
+            labels={Object.values(LBL)}
+            defaultLabels={visibleLabels}
+            onChange={setVisibleLabels}
+          />
+        </Box>
+        <Box display="flex" flexDirection="column" gap={theme.spacing(1)}>
+          <SectionTitle>Definitions</SectionTitle>
+          <ConditionalRenderList visibleLabels={visibleLabels}>
+            <ConditionalRenderListItem labels={[LBL.FN]}>
+              <p>
+                In the following definitions, let $p \in \R$ be a point
+                and $f(x) : \R \to \R$ be a function defined on some
+                open interval left to be specified. Where appropriate,{" "}
+                $x_0 \in \R$ is the endpoint of an interval on which{" "}
+                $f$ is defined.
+              </p>
+              <PartTitle>Limits at a Point</PartTitle>
+              <p>
+                Let $f$ be defined on some open interval around, but
+                not necessarily including $p$. We say that{" "}
+                $L \in \R$ is the <i>limit</i> of $f$ as{" "}
+                $x$ approaches $p$ if for any{" "}
+                {"$\\epsilon > 0$"} there exists some $\delta {">"} 0${" "}
+                so that for any $x \neq p$ and{" "}
+                {"$\\abs{x - p} < \\delta$"} we have{" "}
+                {"$\\abs{f(x) - L} < \\epsilon$"}.
+              </p>
+              <PartTitle>One-Sided Limits</PartTitle>
+              <p>
+                Let $f$ be defined on some open interval{" "}
+                $(x_0, p)$. Then we say that $R \in \R$ is the{" "}
+                <i>limit</i> of $f$ as $x$ approaches{" "}
+                $p$ <b>from the left (or from below)</b> if for any{" "}
+                {"$\\epsilon > 0$"} there exists some {"$\\delta > 0$"}{" "}
+                so that for any {"$x < p$"} and {"$p - x < \\delta$"}{" "}
+                we have {"$\\abs{f(x) - R} < \\epsilon$"}.
+              </p>
+              <p>
+                Let $f$ be defined on some open interval{" "}
+                $(p, x_0)$. Then we say that $L \in \R$ is the{" "}
+                <i>limit</i> of $f$ as $x$ approaches{" "}
+                $p$ <b>from the right (or from above)</b> if for any{" "}
+                {"$\\epsilon > 0$"} there exists some {"$\\delta > 0$"}{" "}
+                so that for any {"$x > p$"} and {"$x - p < \\delta$"}{" "}
+                we have {"$\\abs{f(x) - L} < \\epsilon$"}.
+              </p>
+              <PartTitle>Limits at Infinity</PartTitle>
+              <p>
+                Let $f$ be defined on some interval{" "}
+                $(x_0, \infty)$. Then we say that $L \in \R${" "}
+                is the <i>limit</i> of $f$ as $x$ approaches{" "}
+                $\infty$ if for any {"$\\epsilon > 0$"} there is
+                some $x_\epsilon \in \R$ such that for all{" "}
+                {"$x > x_\\epsilon$"} we have{" "}
+                {"$\\abs{f(x) - L} < \\epsilon$"}
+              </p>
+              <p>
+                Let $f$ be defined on some interval{" "}
+                $(-\infty, x_0)$
+                . Then we say that $L \in \R$ is the <i>limit</i> of{" "}
+                $f$ as $x$ approaches $-\infty$ if
+                for any {"$\\epsilon > 0$"} there is some{" "}
+                $x_\epsilon \in \R$ such that for all{" "}
+                {"$x < x_\\epsilon$"} we have{" "}
+                {"$\\abs{f(x) - L} < \\epsilon$"}
+              </p>
+              <PartTitle>Continuity</PartTitle>
+              <p>
+                $f$ is <i>continuous</i> at point $p$ if
+              </p>
+              <M m="\lim_{x \to p} f(x) = f(p)" />
+              <p>
+                $f$ is continuous on an interval $(a, b)$ if
+                it is continuous at every point $p \in (a, b)$.
+                $f$ is continuous if it is continuous at every point on
+                its domain.
+              </p>
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.SEQ]}>
+              <p>
+                Let {"$X = \\set{x_n}$"} be a sequence of real numbers. We
+                say that $L \in \R$ is the <i>limit</i> of $x${" "}
+                as $n$ approaches $\infty$ and write{" "}
+                {`\[ \lim_{n\to\infty} x_n = L \]`} if for any{" "}
+                {"$\\epsilon > 0$"} there is an $n_0 \in \N$ such
+                that for all {"$n > n_0$"} we have{" "}
+                {"$\\abs{x_n - L} < \\epsilon$"}.
+              </p>
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.MET]}>
+              <p>
+                The definition of the limit on a metric space $\cX${" "}
+                with metric $d : \cX \times \cX \to \R$ mirrors those
+                definitions for limits of regular functions and
+                sequences/series. Instead of assuming{" "}
+                {"$d(x, y) = \\abs{x - y}$"} as is the case for{" "}
+                $\R$ , we allow $d$ to be an arbitrary metric.
+              </p>
+              <PartTitle>Functions</PartTitle>
+              <p>
+                Let $f$ be defined on some open interval around, but
+                not necessarily including $p$. We say that{" "}
+                $L \in \cX$ is the <i>limit</i> of $f$ as{" "}
+                $x$ approaches $p$ if for any{" "}
+                {"$\\epsilon > 0$"} there exists some {"$\\delta > 0$"}{" "}
+                so that for any $x \neq p$ and{" "}
+                {"$\\abs{x - p} < \\delta$"} we have{" "}
+                {"$d(f(x), L) < \\epsilon$"}.
+              </p>
+              <PartTitle>Sequences</PartTitle>
+              <p>
+                Let {"$X = \\set{x_n}$"} be a sequence of elements in{" "}
+                $\cX$. We say that $L \in \cX$ is the{" "}
+                <i>limit</i> of $x$ as $n$ approaches{" "}
+                $\infty$ and write
+              </p>
+              <M m="\lim_{n\to\infty} x_n = L" />
+              <p>
+                if for any {"$\\epsilon > 0$"} there is an{" "}
+                $n_0 \in \N$ such that for all {"$n > n_0$"} we
+                have {"$d(x_n, L) < \\epsilon$"}.
+              </p>
+            </ConditionalRenderListItem>
+          </ConditionalRenderList>
 
-        <SectionTitle>Limit Arithmetic</SectionTitle>
-        <ConditionalRenderList visibleLabels={visibleLabels}>
-          <ConditionalRenderListItem labels={[LBL.FN]}>
-            <p>
-              Let <IM m="c \in \R" /> be a constant,{" "}
-              <IM m="p \in \R \cup \set{\infty, -\infty}" /> be a point, and{" "}
-              <IM m="f, g : \R \to \R" /> be functions such that{" "}
-              <IM m="\lim_{x \to p} f(x) = L_f" /> and{" "}
-              <IM m="\lim_{x \to p} g(x) = L_g" />. It is true that:
-            </p>
-            <M
-              m="
+          <SectionTitle>Limit Arithmetic</SectionTitle>
+          <ConditionalRenderList visibleLabels={visibleLabels}>
+            <ConditionalRenderListItem labels={[LBL.FN]}>
+              <p>
+                Let $c \in \R$ be a constant,{" "}
+                {"$p \\in \\R \\cup \\set{\\infty, -\\infty}$"} be a point, and{" "}
+                $f, g : \R \to \R$ be functions such that{" "}
+                {"$\\lim_{x \\to p} f(x) = L_f$"} and{" "}
+                {"$\\lim_{x \\to p} g(x) = L_g$"}. It is true that:
+              </p>
+              <M
+                m="
                 \begin{align}
                   \lim_{x \to p} [f(x) + g(x)] &= L_f + L_g \\
                   \lim_{x \to p} [cf(x)] &= cL_f \\
@@ -170,17 +182,17 @@ export default function LimitsPage() {
                   \lim_{x \to p} f^c(x) &= L_f^c &(L_f > 0)
                 \end{align}
               "
-            />
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.SEQ]}>
-            <p>
-              Let <IM m="c \in \R" /> be a constant,{" "}
-              <IM m="\set{x_n}, \set{y_n}" /> be sequences such that{" "}
-              <IM m="\lim_{n \to \infty} x_n = L_x" /> and{" "}
-              <IM m="\lim_{n \to \infty} y_n = L_y" />. It is true that:
-            </p>
-            <M
-              m="
+              />
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.SEQ]}>
+              <p>
+                Let $c \in \R$ be a constant,{" "}
+                {"$\\set{x_n}, \\set{y_n}$"} be sequences such that{" "}
+                {"$\\lim_{n \\to \\infty} x_n = L_x$"} and{" "}
+                {"$\\lim_{n \\to \\infty} y_n = L_y$"}. It is true that:
+              </p>
+              <M
+                m="
                 \begin{align}
                   \lim_{n \to \infty} [x_n + y_n] &= L_x + L_y \\
                   \lim_{n \to \infty} [cx_n] &= cL_x \\
@@ -189,40 +201,41 @@ export default function LimitsPage() {
                   \lim_{n \to \infty} x_n^c &= L_x^c &(L_x > 0)
                 \end{align}
               "
-            />
-          </ConditionalRenderListItem>
-        </ConditionalRenderList>
+              />
+            </ConditionalRenderListItem>
+          </ConditionalRenderList>
 
-        <SectionTitle>Theorems</SectionTitle>
-        <ConditionalRenderList visibleLabels={visibleLabels}>
-          <ConditionalRenderListItem labels={[LBL.FN]}>
-            TODO: Theorems for limits for functions
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.SEQ]}>
-            TODO: Theorems for limits for sequences
-          </ConditionalRenderListItem>
-        </ConditionalRenderList>
+          <SectionTitle>Theorems</SectionTitle>
+          <ConditionalRenderList visibleLabels={visibleLabels}>
+            <ConditionalRenderListItem labels={[LBL.FN]}>
+              TODO: Theorems for limits for functions
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.SEQ]}>
+              TODO: Theorems for limits for sequences
+            </ConditionalRenderListItem>
+          </ConditionalRenderList>
 
-        <SectionTitle>Convergence</SectionTitle>
-        <ConditionalRenderList visibleLabels={visibleLabels}>
-          <ConditionalRenderListItem labels={[LBL.FN]}>
-            TODO: Convergence of limits for functions
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.SEQ]}>
-            TODO: Convergence of limits for sequences
-          </ConditionalRenderListItem>
-          <ConditionalRenderListItem labels={[LBL.PROB]}>
-            TODO: Convergence in distribution, probability, almost sure
-          </ConditionalRenderListItem>
-        </ConditionalRenderList>
+          <SectionTitle>Convergence</SectionTitle>
+          <ConditionalRenderList visibleLabels={visibleLabels}>
+            <ConditionalRenderListItem labels={[LBL.FN]}>
+              TODO: Convergence of limits for functions
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.SEQ]}>
+              TODO: Convergence of limits for sequences
+            </ConditionalRenderListItem>
+            <ConditionalRenderListItem labels={[LBL.PROB]}>
+              TODO: Convergence in distribution, probability, almost sure
+            </ConditionalRenderListItem>
+          </ConditionalRenderList>
 
-        <SectionTitle>Relationships Between Limits</SectionTitle>
-        <ConditionalRenderList visibleLabels={visibleLabels}>
-          <ConditionalRenderListItem labels={[LBL.FN, LBL.SEQ]}>
-            TODO: Relationship between functional and sequence limits
-          </ConditionalRenderListItem>
-        </ConditionalRenderList>
-      </Box>
+          <SectionTitle>Relationships Between Limits</SectionTitle>
+          <ConditionalRenderList visibleLabels={visibleLabels}>
+            <ConditionalRenderListItem labels={[LBL.FN, LBL.SEQ]}>
+              TODO: Relationship between functional and sequence limits
+            </ConditionalRenderListItem>
+          </ConditionalRenderList>
+        </Box>
+      </TypesetKatex>
     </PageContainer>
   );
 }
--- src/components/MyKatex.tsx
+++ src/components/MyKatex.tsx
@@ -2,7 +2,7 @@
 
 import { Box, useTheme } from "@mui/material";
 import Katex from "katex";
-import { createContext, ReactNode, useContext, useMemo } from "react";
+import React, { createContext, ReactNode, useContext, useMemo } from "react";
 
 const _KatexContext = createContext({});
 
@@ -44,4 +44,52 @@ export function M(props: { m: string }) {
 
 export function IM(props: { m: string }) {
   return <KatexComponent math={props.m} inline={true} />;
+}
+
+interface TypesetKatexProps {
+  children?: ReactNode;
+}
+
+function typesetKatexInString(s: string) {
+  const parts: ReactNode[] = [];
+  let math = false;
+  let startI = 0;
+  for (let i = 0; i < s.length; ++i) {
+    if (s[i] === "$" && s[i - 1] !== "\\") {
+      if (i !== startI) {
+        if (math) {
+          parts.push(<IM key={i} m={s.substring(startI, i)} />);
+        } else {
+          parts.push(s.substring(startI, i));
+        }
+      }
+      math = !math;
+      startI = i + 1;
+    }
+  }
+  if (math) {
+    parts.push(<IM m={s.substring(startI)} />);
+  } else {
+    parts.push(s.substring(startI));
+  }
+  return parts;
+}
+
+function typesetKatexInNodeRecursively(node: ReactNode, key?: number): ReactNode {
+  if (typeof node === "string") {
+    return typesetKatexInString(node);
+  } else if (React.isValidElement<{ children?: React.ReactNode }>(node)) {
+    return React.cloneElement(node, {
+      children: typesetKatexInNodeRecursively(node.props.children),
+      key: node.key || key
+    });
+  } else if (Array.isArray(node)) {
+    return node.map(typesetKatexInNodeRecursively);
+  } else {
+    return node;
+  }
+}
+
+export function TypesetKatex({ children }: TypesetKatexProps) {
+  return typesetKatexInNodeRecursively(children);
 }
